22c623a97380e5ffc30d08a68013fe1aeea330c3
sho171 <sh.zxcv00@gmail.com>
2020-10-28 09:44:46 +0900

    Fix: レビュー内容修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     deadline { Random.rand(from..to) }
 
     trait :done do
-      status { "done" }
+      status { :done }
       completion_date { Time.current.yesterday }
     end
   end
